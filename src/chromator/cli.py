c30fa4bf4bad6af27ee3c49ae56648b5fa2dbc76
Huib Piguillet <huib@protonmail.com>
2025-06-11 01:28:27 +0200

    Cleaned up code

--- src/chromator/cli.py
+++ src/chromator/cli.py
@@ -4,7 +4,27 @@ import sys
 from hsluv import hex_to_hsluv, hsluv_to_hex
 from yachalk import chalk
 
-from .colors import shades
+from .colors import HSLuv, shades
+
+
+def contrasting_color(color: HSLuv) -> HSLuv:
+    hue, saturation, lightness = color
+    return hue, saturation, (lightness + 50) % 100
+
+
+def colored(color: HSLuv, s: str = None):
+    bg_hex = hsluv_to_hex(color)
+    fg_hex = hsluv_to_hex(contrasting_color(color))
+    return chalk.hex(fg_hex).bg_hex(bg_hex)(s or bg_hex)
+
+
+def css_color_comment(color: HSLuv):
+    hue, saturation, lightness = color
+    return f"""
+{colored(color)}:
+- Hue: {hue:.1f}°
+- Saturation: {saturation:.1f}%
+- Lightness: {lightness:.1f}%"""
 
 
 def print_shades_css(
@@ -14,59 +34,36 @@ def print_shades_css(
         step: int = 5,
         extrapolate: float = 0,
 ) -> None:
-    h_1, s_1, l_1 = color_1 = hex_to_hsluv(f"#{hex_1}")
-    hex_1_fg = hsluv_to_hex((h_1, s_1, (l_1 + 50) % 100))
+    color_1 = hex_to_hsluv(f"#{hex_1}")
 
     if hex_2:
-        h_2, s_2, l_2 = color_2 = hex_to_hsluv(f"#{hex_2}")
-        hex_2_fg = hsluv_to_hex((h_2, s_2, (l_2 + 50) % 100))
+        color_2 = hex_to_hsluv(f"#{hex_2}")
         sys.stdout.write(f"""/*
 Based on:
-
-#{chalk.hex(hex_1_fg).bg_hex(hex_1)(hex_1)}:
-- Hue: {h_1:.1f}°
-- Saturation: {s_1:.1f}%
-- Lightness: {l_1:.1f}%
-
-#{chalk.hex(hex_2_fg).bg_hex(hex_2)(hex_2)}:
-- Hue: {h_2:.1f}°
-- Saturation: {s_2:.1f}%
-- Lightness: {l_2:.1f}%
+{css_color_comment(color_1)}
+{css_color_comment(color_2)}
 */
 """)
 
     else:
         color_2 = None
         sys.stdout.write(f"""/*
-Based on #{chalk.hex(hex_1_fg).bg_hex(hex_1)(hex_1)}:
-- Hue: {h_1:.1f}°
-- Saturation: {s_1:.1f}%
-- Lightness: {l_1:.1f}%
+Based on:
+{css_color_comment(color_1)}
 */
 """)
 
-    for (hue, sat, i) in shades(color_1, color_2, step, extrapolate):
-        new_hex_bg = hsluv_to_hex((hue, sat, i))
-        new_hex_fg = hsluv_to_hex((hue, sat, (i + 50) % 100))
-        sys.stdout.write(chalk.hex(new_hex_fg).bg_hex(new_hex_bg)(
-            f"""--{label}-{i:03d}: {new_hex_bg};
-"""))
+    for h, s, i in shades(color_1, color_2, step, extrapolate / 100):
+        color_var = f"--{label}-{i:03d}: {hsluv_to_hex((h, s, i))};\n"
+        sys.stdout.write(colored((h, s, i), color_var))
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("label", type=str)
-    parser.add_argument("-c1", "--color1", type=str)
-    parser.add_argument("-c2", "--color2", type=str, default=None)
+    parser.add_argument("-c", "--color1", type=str)
+    parser.add_argument("-k", "--color2", type=str, default=None)
     parser.add_argument("-s", "--step", type=int, default=5)
     parser.add_argument("-e", "--extrapolate", type=int, default=0)
     args = parser.parse_args()
-
-    # chalk.set_color_mode(chalk.get_color_mode())
-    print_shades_css(
-        args.label,
-        args.color1,
-        args.color2,
-        args.step,
-        args.extrapolate / 100,
-    )
+    print_shades_css(args.label, args.color1, args.color2, args.step, args.extrapolate)
