0d6a906957a92b7f96285db327317855d9c7d8d2
Huib Piguillet <huib@protonmail.com>
2025-11-20 09:50:30 +0100

    cleanup color theme code

--- src/chromator/color_theme.py
+++ src/chromator/color_theme.py
@@ -2,35 +2,38 @@ from typing import TYPE_CHECKING
 
 from based_utils.calx import fractions
 from based_utils.cli import Colored
-from based_utils.colors import COLORS, HUES, Color, Hues
+from based_utils.colors import HUES, Color, Hues
 
 if TYPE_CHECKING:
-    from collections.abc import Iterator
+    from collections.abc import Iterable, Iterator
 
 
-def _color_shades(c: Color) -> Iterator[str]:
-    for f in fractions(19):
-        s = c.shade(f)
-        yield Colored(f" {s.hex} ", s.contrasting_shade, s).formatted
+def _color_shade(color: Color) -> str:
+    return Colored(f" {color.hex} ", color.contrasting_shade, color).formatted
 
 
-def _color_line(c: Color, n: str = "") -> str:
-    h = "   " if n == "grey" else f"{c.hue * 360:03.0f}"
-    return "".join(_color_shades(c)) + Colored(f" {h} {n}", c).formatted
-
-
-def _saturation_lines(saturation: float, color_cls: type[Color]) -> Iterator[str]:
-    if saturation == 0:
-        yield _color_line(color_cls.grey(), "grey")
-    else:
-        yield "".join(f"   {f * 100:.0f}%  " for f in fractions(19))
-        for h in COLORS:
-            yield _color_line(color_cls.from_name(h, saturation=saturation), h)
+def _color_line(color: Color, name: str, shades: Iterable[float]) -> str:
+    shades_str = "".join(_color_shade(color.shade(s)) for s in shades)
+    hue = "   " if name == "grey" else f"{color.hue * 360:03.0f}"
+    return shades_str + Colored(f" {hue} {name}", color).formatted
 
 
 def color_lines(custom_hues: Hues) -> Iterator[str]:
     class CustomColor(Color):
         hues = HUES | custom_hues
 
-    for saturation in fractions(3, inclusive=True):
-        yield from _saturation_lines(saturation, CustomColor)
+    shades = list(fractions(19))
+
+    # Any color will be grey when saturation is set to 0.
+    yield _color_line(CustomColor.grey(), "grey", shades)
+
+    # Shade/hue tables for specific saturation values
+    num_saturations = 4
+    for i in range(1, num_saturations + 1):
+        # Shade percentages row
+        yield "".join(f"   {s * 100:.0f}%  " for s in shades)
+        # Tables
+        saturation = i / num_saturations
+        for name in HUES:
+            color = CustomColor.from_name(name, saturation=saturation)
+            yield _color_line(color, name, shades)
