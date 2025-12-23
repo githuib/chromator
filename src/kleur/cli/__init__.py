7d3fab68e737093c90853767664ea8498cb13383
Huib Piguillet <huib@protonmail.com>
2025-12-23 19:17:56 +0100

    try again

--- src/kleur/cli/__init__.py
+++ src/kleur/cli/__init__.py
@@ -1,8 +1,16 @@
-from based_utils.cli import run_command
+import sys
 
 from .palette_gen import PaletteGenerator
 from .shades_gen import ShadesGenerator
 
+try:
+    from based_utils.cli import run_command
+except ImportError:
+    run_command = None
+
 
 def main() -> None:
-    run_command(PaletteGenerator, ShadesGenerator)
+    if run_command:
+        run_command(PaletteGenerator, ShadesGenerator)
+    else:
+        sys.exit("Install package as 'kleur[cli]' to use any console scripts in ot.")
